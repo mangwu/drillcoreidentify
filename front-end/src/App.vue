7c881a9ef30eeefbea78a8bcfdf2ed4a39ba87ff
mangwu <1185956753@qq.com>
2021-04-11 02:01:26 +0800

    2.axios

---
+++ front-end/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+
+@import "./assets/css/base.css";
+
+#app {
+  font-family: '微软雅黑', 'Avenir', Helvetica, Arial, sans-serif;
+  
+}
+</style>
